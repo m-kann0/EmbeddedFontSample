91e55161868bd9c5a8365a5e858fb22bbebccc37
m-kann0 <m.kanno.0915@gmail.com>
2018-11-20 00:42:59 +0900

    画面遷移と遷移先のWebView実装

--- app/src/main/java/com/sample/font/MainActivity.java
+++ app/src/main/java/com/sample/font/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sample.font;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnShow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sample/font/WebViewActivity.java
@@ -0,0 +1,17 @@
+package com.sample.font;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/content.html");
+    }
+}
