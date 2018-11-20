d237f2eab4e38287662ba306bc040e40adb5468c
m-kann0 <m.kanno.0915@gmail.com>
2018-11-20 20:56:58 +0900

    WebViewのアクセス先を任意に入力できるように修正

--- app/src/main/java/com/sample/font/MainActivity.java
+++ app/src/main/java/com/sample/font/MainActivity.java
@@ -4,20 +4,43 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String INITIAL_URL = "file:///android_asset/content.html";
+
+    private EditText editUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editUrl = findViewById(R.id.editUrl);
+
+        findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetUrl();
+            }
+        });
+
         findViewById(R.id.btnShow).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String url = editUrl.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra(WebViewActivity.EXTRA_URL, url);
                 startActivity(intent);
             }
         });
+
+        resetUrl();
+    }
+
+    private void resetUrl() {
+        editUrl.setText(INITIAL_URL);
     }
 }
--- app/src/main/java/com/sample/font/WebViewActivity.java
+++ app/src/main/java/com/sample/font/WebViewActivity.java
@@ -1,17 +1,23 @@
 package com.sample.font;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    public static final String EXTRA_URL = "com.sample.font.WebViewActivity.URL";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
+        Intent intent = getIntent();
+        String url = intent.getStringExtra(EXTRA_URL);
+
         WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("file:///android_asset/content.html");
+        webView.loadUrl(url);
     }
 }
