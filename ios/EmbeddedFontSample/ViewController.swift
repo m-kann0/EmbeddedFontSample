4870f04140d6c8f24fc19d7ab5083c2f8d78edcc
m-kann0 <m.kanno.0915@gmail.com>
2018-11-23 00:35:29 +0900

    iOSプロジェクトを追加

---
+++ ios/EmbeddedFontSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmbeddedFontSample
+//
+//  Created by Masato Kanno on 2018/11/23.
+//  Copyright © 2018 Masato Kanno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
